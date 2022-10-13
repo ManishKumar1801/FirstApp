7de7cd5f1948116aeaaf265181f5bad1946338b5
Nitesh Kumar <niteshman30@gmail.com>
2022-10-13 16:42:55 +0530

    changes

--- app/src/main/java/com/example/kabir/MainActivity.kt
+++ app/src/main/java/com/example/kabir/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.kabir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.kabir.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       binding=DataBindingUtil. setContentView(this,R.layout.activity_main)
+
+        with(binding){
+
+        }
 
     }
 }
