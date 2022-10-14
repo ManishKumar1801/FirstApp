96adc0a58283be7995cf84e515a3a81b7dee0b17
Manish <manishkumaryadavcse@gmail.com>
2022-10-14 16:19:52 +0530

    First commit

---
+++ app/src/main/java/com/example/kabir/DetailActivity.kt
@@ -0,0 +1,22 @@
+package com.example.kabir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.kabir.databinding.ActivityDetailBinding
+import com.example.kabir.databinding.ActivityMainBinding
+
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_detail)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_detail)
+
+        with(binding){
+            Discription.text="ajay devgani s veryg ood avtor hei s nerysk fjfus kdkfkjj sgdtyrhfn jksjfjfkfk"
+        }
+    }
+}
--- app/src/main/java/com/example/kabir/MainActivity.kt
+++ app/src/main/java/com/example/kabir/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.kabir
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.kabir.databinding.ActivityMainBinding
 
@@ -12,6 +14,27 @@ class MainActivity : AppCompatActivity() {
        binding=DataBindingUtil. setContentView(this,R.layout.activity_main)
 
         with(binding){
+
+            Ajay.setOnClickListener {
+                Toast.makeText(this@MainActivity, "click on Ajay", Toast.LENGTH_SHORT).show()
+                val intent=Intent(this@MainActivity,DetailActivity::class.java)
+                startActivity(intent)
+            }
+            Akshay.setOnClickListener {
+                Toast.makeText(this@MainActivity, "click on Akshy", Toast.LENGTH_SHORT).show()
+                val intent=Intent(this@MainActivity,DetailActivity::class.java)
+                startActivity(intent)
+            }
+            Salman.setOnClickListener {
+                Toast.makeText(this@MainActivity, "click on Salman ", Toast.LENGTH_SHORT).show()
+                val intent=Intent(this@MainActivity,DetailActivity::class.java)
+                startActivity(intent)
+            }
+            Sahurakh.setOnClickListener {
+                Toast.makeText(this@MainActivity, "click on Sahrukh", Toast.LENGTH_SHORT).show()
+                val intent=Intent(this@MainActivity,DetailActivity::class.java)
+                startActivity(intent)
+            }
 
         }
 
